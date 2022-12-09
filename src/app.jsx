55a38eb01df9f865f2cfddf09a947b1288b71a47
franciscocaballero <caballerofrancisco22@yahoo.com>
2022-12-08 19:00:17 -0500

    adjusting grid column and row layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,12 @@
 import React, { useRef, useEffect, useState } from "react";
-
 import Map from "react-map-gl";
 import ReactMapGL, { Marker } from "react-map-gl";
-
 import { Nav } from "./components/nav";
 import { Grid } from "./components/grid";
-
-const MAPBOX_TOKEN =
-  "pk.eyJ1IjoiY2lzY29jYWJhbGxlcm8iLCJhIjoiY2xiY3VlMHU3MDc2azNubzJndHpmMWxwNCJ9.UQO39BzfgajlPk6aXuSqcw"; // Set your mapbox token here
 
 const App = () => {
   return (
     <div>
-      <Map
-        initialViewState={{
-          latitude: 38.8915,
-          longitude: -77.0408,
-          zoom: 12,
-        }}
-        style={{ width: 800, height: 600 }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        mapboxAccessToken={MAPBOX_TOKEN}
-      >
-        <Marker longitude={-77.0408} latitude={38.8915} anchor="bottom">
-          <img
-            className="me"
-            src="images/me.png"
-            alt="Profile"
-            style={{ width: "20px" }}
-          />
-        </Marker>
-      </Map>
       <Nav />
       <Grid />
     </div>
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import Map from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
+
+const MAPBOX_TOKEN =
+  "pk.eyJ1IjoiY2lzY29jYWJhbGxlcm8iLCJhIjoiY2xiY3VlMHU3MDc2azNubzJndHpmMWxwNCJ9.UQO39BzfgajlPk6aXuSqcw";
 
 export const Grid = () => {
+  const [viewState, setViewState] = React.useState({
+    longitude: -77.0408,
+    latitude: 38.8915,
+    zoom: 13.5,
+  });
   return (
     <div className="container">
       <div className="grid">
-        <div className="grid-item grid-col-span-2">
-          <div className="bio-container radius flex-colum">
-            <div className="bio-container__image">
-              <img className="me" src="images/me.png" alt="Profile" />
-            </div>
-            <p>
-              Hi👋🏼 I'm{" "}
-              <span style={{ fontSize: "30px", fontWeight: "bold" }}>
-                Francisco
-              </span>
-              , a developer from Arlington VA. I'm interested in Javascript,
-              React, Reading, Health, Jiu jitsu, Design, and all things Web
-              developement.
-            </p>
+        <div className="bio-container radius flex-colum">
+          <div className="bio-container__image">
+            <img className="me" src="images/me.png" alt="Profile" />
           </div>
+          <p className="bio-container__bio">
+            Hi👋🏼 I'm{" "}
+            <span style={{ fontSize: "30px", fontWeight: "bold" }}>
+              Francisco
+            </span>
+            , a developer from Arlington VA. I'm interested in Javascript,
+            React, Reading, Health, Jiu jitsu, Design, and all things Web
+            developement.
+          </p>
         </div>
         <div className="grid-item grid-col-span-1">
           <div className=" social-item-container social-item-container__twitter radius flex">
@@ -38,7 +46,26 @@ export const Grid = () => {
             />
           </div>
         </div>
-        <div className="grid-item grid-row-span-2">Hello</div>
+        <div className="grid-item grid-row-span-1">
+          <div className="radius">
+            <Map
+              {...viewState}
+              onMove={(evt) => setViewState(evt.viewState)}
+              mapStyle="mapbox://styles/mapbox/streets-v9"
+              style={{ width: 300, height: 300 }}
+              mapboxAccessToken={MAPBOX_TOKEN}
+            >
+              <Marker longitude={-77.0408} latitude={38.8915} anchor="bottom">
+                <img
+                  className="me"
+                  src="images/me.png"
+                  alt="Profile"
+                  style={{ width: "20px" }}
+                />
+              </Marker>
+            </Map>
+          </div>
+        </div>
         <div className="item-1">Hello</div>
       </div>
     </div>
