dc4e8b1f5e13b3b0dbb4e7f1ef9b768858701320
franciscocaballero <caballerofrancisco22@yahoo.com>
2022-12-12 22:32:57 -0500

    updaitng nav menu

---
+++ src/components/MenuItems.js
@@ -0,0 +1,6 @@
+export const MenuItems = [
+  { title: "Home", url: "#", cName: "nav-menu_option" },
+  { title: "About", url: "#", cName: "nav-menu_option" },
+  { title: "Projects", url: "#", cName: "nav-menu_option" },
+  { title: "Media", url: "#", cName: "nav-menu_option" },
+];
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,19 +1,47 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { MenuItems } from "./MenuItems";
 export const Nav = () => {
+  // const [show, setShow] = useState(false);
+  // const [show2, setShow2] = useState(false);
+  // const [show3, setShow3] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const [isActive2, setIsActive2] = useState(false);
+  const [isActive3, setIsActive3] = useState(false);
+  const [isActive4, setIsActive4] = useState(false);
+
+  const handleClick = (e) => {
+    console.log(e);
+    setIsActive((current) => !current);
+  };
+
+  const onToggleClick = () => setIsActive(!isActive);
+  const onToggleClick2 = () => setIsActive2(!isActive2);
+  const onToggleClick3 = () => setIsActive3(!isActive3);
+  const onToggleClick4 = () => setIsActive4(!isActive4);
+
+  let toggleCheck = isActive ? " active" : "";
+  console.log(isActive);
   return (
     <div>
       <nav>
         <p className="nav--logo">Cisco</p>
 
         <ul className="nav-menu">
-          <li className="nav-menu_option">All</li>
-          <li className="nav-menu_option">About</li>
-          <li className="nav-menu_option">Projects</li>
-          <li className="nav-menu_option nav-menu_option--hightlight">Media</li>
+          <li className="1" onClick={handleClick}>
+            Home
+          </li>
+          <li className="1" onClick={handleClick}>
+            Home
+          </li>
+          <li className="1" onClick={handleClick}>
+            Home
+          </li>
+          <li className="1" onClick={handleClick}>
+            Home
+          </li>
         </ul>
         <a
-          href="mailto: caballerofrancisco22@yahoo.com"
+          href="mailto:caballerofrancisco22@yahoo.com"
           target="_blank”"
           className="btn--contact"
         >
