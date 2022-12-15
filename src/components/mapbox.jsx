cc1d06f07ce8ec47bbdb3d4ae5d60f04dd741957
franciscocaballero <caballerofrancisco22@yahoo.com>
2022-12-14 22:11:31 -0500

    updaing map component

--- src/components/mapbox.jsx
+++ src/components/mapbox.jsx
@@ -1,6 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
-import Map from "react-map-gl";
-import ReactMapGL, { Marker } from "react-map-gl";
+import { render } from "react-dom";
+import Map, { Marker } from "react-map-gl";
+
+// import "mapbox-gl/dist/mapbox-gl.css";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiY2lzY29jYWJhbGxlcm8iLCJhIjoiY2xiY3VlMHU3MDc2azNubzJndHpmMWxwNCJ9.UQO39BzfgajlPk6aXuSqcw";
@@ -9,27 +11,38 @@ export const Mapbox = () => {
   const [viewState, setViewState] = React.useState({
     longitude: -77.0408,
     latitude: 38.8915,
-    zoom: 13.5,
+    zoom: 10.5,
   });
+
+  const hello = () => {
+    console.log("hello");
+  };
   return (
     <div className="container--mapbox">
       <Map
         className="container"
         {...viewState}
         onMove={(evt) => setViewState(evt.viewState)}
+        // onMove={hello}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         style={{ width: 400, height: 400 }}
         mapboxAccessToken={MAPBOX_TOKEN}
       >
-        <Marker longitude={-77.0408} latitude={38.8915} anchor="bottom">
+        <Marker
+          // longitude={viewState.longitude}
+          // latitude={viewState.latitude}
+          {...viewState}
+          style={{ transform: "translate(0)" }}
+          anchor="bottom"
+        >
           <img
             src="images/me.png"
             alt="Mapmark Image"
-            style={{ width: "20px" }}
+            style={{ width: "30px" }}
           />
         </Marker>
       </Map>
-      <a className="btn btn--white" target="_blank”" href="#">
+      <a className="btn btn--white" target="_blank”" href="#" onClick={hello}>
         <img className="btn__icon" src="images/zoom-out.png" alt="minus" />
       </a>
     </div>
