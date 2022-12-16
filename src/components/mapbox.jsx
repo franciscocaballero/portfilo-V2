ba13d7e45077a6bffb57f8867644cffeb986d001
franciscocaballero <caballerofrancisco22@yahoo.com>
2022-12-15 19:00:38 -0500

    updating map controls

--- src/components/mapbox.jsx
+++ src/components/mapbox.jsx
@@ -1,8 +1,5 @@
-import React, { useRef, useEffect, useState } from "react";
-import { render } from "react-dom";
-import Map, { Marker } from "react-map-gl";
-
-// import "mapbox-gl/dist/mapbox-gl.css";
+import React from "react";
+import Map, { Marker, NavigationControl } from "react-map-gl";
 
 const MAPBOX_TOKEN =
   "pk.eyJ1IjoiY2lzY29jYWJhbGxlcm8iLCJhIjoiY2xiY3VlMHU3MDc2azNubzJndHpmMWxwNCJ9.UQO39BzfgajlPk6aXuSqcw";
@@ -11,39 +8,49 @@ export const Mapbox = () => {
   const [viewState, setViewState] = React.useState({
     longitude: -77.0408,
     latitude: 38.8915,
-    zoom: 10.5,
+    zoom: 13,
   });
 
-  const hello = () => {
-    console.log("hello");
-  };
   return (
     <div className="container--mapbox">
       <Map
         className="container"
         {...viewState}
         onMove={(evt) => setViewState(evt.viewState)}
-        // onMove={hello}
+        // setViewState(evt.viewState);
+        // console.log(evt);
+
+        dragPan={false}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         style={{ width: 400, height: 400 }}
         mapboxAccessToken={MAPBOX_TOKEN}
+        // minZoom={10}
+        maxZoom={18}
+        // touchPitch={false}
+        // touchZoomRotate={!true}
       >
-        <Marker
-          // longitude={viewState.longitude}
-          // latitude={viewState.latitude}
-          {...viewState}
-          style={{ transform: "translate(0)" }}
-          anchor="bottom"
-        >
+        <Marker longitude={-77.0408} latitude={38.8915} scale={0}>
           <img
             src="images/me.png"
             alt="Mapmark Image"
-            style={{ width: "30px" }}
+            style={{ width: "40px" }}
           />
         </Marker>
+        <NavigationControl showCompass={false} />
       </Map>
-      <a className="btn btn--white" target="_blank”" href="#" onClick={hello}>
+      <a
+        className="btn btn--white"
+        href="#"
+        onClick={() => setViewState({ zoom: 9 })}
+      >
         <img className="btn__icon" src="images/zoom-out.png" alt="minus" />
+      </a>
+      <a
+        className="btn btn--zoom"
+        href="#"
+        onClick={() => setViewState({ zoom: 13 })}
+      >
+        <img className="btn__icon" src="images/plus3.png" alt="minus" />
       </a>
     </div>
   );
